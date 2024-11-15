4ac27d0e8e48c5fddefa761d7b006d533c081ba0
sofiahdingdong <sofiahdingdong@gmail.com>
2024-11-15 11:09:50 +0700

    Percobaan 1 modif

--- ArrayBilangan21.java
+++ ArrayBilangan21.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 public class ArrayBilangan21 {
     public static void main(String[] args) {
-        int[] bil = new int[4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        double[] bil = new double[4];
+        bil[0] = 5.0;
+        bil[1] = 12867;
+        bil[2] = 7.5;
+        bil[3] = 2000000;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for(int i = 0; i <= 4; i++){
+            System.out.println(bil[i]);
+        }
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
     }
 }
